e478a3845079ad5e12735c18ba7fad1c7ea93808
unknown <tom_1237@hotmail.com>
2021-01-19 22:00:23 +0000

    first commit

---
+++ app/src/main/java/tom/eyre/crimehub/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package tom.eyre.crimehub.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tom.eyre.crimehub.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
